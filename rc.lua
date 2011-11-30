91a33727faa2bde3b114aee162b6eb09a1024033
Daniel Anthofer <daniel-anthofer@web.de>
2011-11-30 10:23:00 +0100

    add comment reminder requesting dynamic tagging

--- rc.lua
+++ rc.lua
@@ -48,6 +48,7 @@ require("vicious")
 --#############################################################################
 -- Tags {{{
 	-- Define a tag table which hold all screen tags. We only have one screen.
+	-- TODO try out shifty and eminent for dynamic tagging
 	tags = {}
 	tags[myscreen]=awful.tag({"web","msg","fm","dev","msc"}, s, layouts[1])
 -- }}}
