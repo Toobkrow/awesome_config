49647c75aac4f44dad6cb9b653224ddb763ed304
Daniel Anthofer <daniel-anthofer@web.de>
2012-04-04 15:55:00 +0200

    theme changed

--- theme/theme.lua
+++ theme/theme.lua
@@ -1,20 +1,20 @@
 theme = {}
 
-theme.font          = "monospace 8"
+theme.font          = "terminus 8"
 
 theme.bg_normal     = "#000000"
-theme.bg_focus      = "#8B0000"
+theme.bg_focus      = "#000000"
 theme.bg_urgent     = "#aa0000"
 theme.bg_minimize   = "#aa0000"
 
-theme.fg_normal 	= "#ffffff"
+theme.fg_normal 	= "#777777"
 theme.fg_focus 		= "#ffffff"
 theme.fg_urgent   = "#ffffff"
 theme.fg_minimize   = theme.fg_normal
 
-theme.border_width  = "4"
+theme.border_width  = "1"
 theme.border_normal = theme.bg_normal
-theme.border_focus  = theme.bg_focus
+theme.border_focus  = theme.fg_normal
 theme.border_marked = theme.bg_urgent
 
 -- Display the taglist squares
